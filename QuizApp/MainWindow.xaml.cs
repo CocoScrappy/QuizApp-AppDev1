7f0ceed67a0da8b652e9234514b7d6025fca7f16
Louis <louis.dumasv@gmail.com>
2022-10-16 09:53:36 -0400

    Adding code behind for button

--- QuizApp/MainWindow.xaml.cs
+++ QuizApp/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace QuizApp
 {
@@ -20,9 +24,20 @@ namespace QuizApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        HttpClient client = new HttpClient();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DlgCreateTest inputDialog = new DlgCreateTest();
+            if (inputDialog.ShowDialog() == true)
+                BtnCreateDialog.Content = "Success!";
+        }
+
+          
     }
+
 }
